2014c58135699b5bd16ec5b680382e665fe90f51
Kevlar2222 <kevin.satti@hotmail.co.uk>
2020-05-07 18:54:50 +0800

    palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    string = string.toLowerCase();
+    string = string.replace(/[^a-z]/g, "");
+    let holder = string.split("");
+    holder = holder.reverse();
+    if(holder.join("") == string){
+        return(true);
+    }
+    else{
+        return(false);
+    }
 }
 
 module.exports = palindromes
