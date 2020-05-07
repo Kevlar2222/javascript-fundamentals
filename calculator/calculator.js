c2ce20c77e60553f325bc6cfae0f3d19daa8f793
Kevlar2222 <kevin.satti@hotmail.co.uk>
2020-05-07 18:30:03 +0800

    extra directory

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,39 @@
-function add () {
-	
+function add (a, b) {
+	return(a + b);	
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return(a - b);	
 }
 
-function sum () {
-	
+function sum (array) {
+	let total = 0;
+	array.forEach(value => total += value);
+	return total;		
 }
 
-function multiply () {
-	
+function multiply (array) {
+	let total = 1;
+	array.forEach(value => total *= value);
+	return total;	
 }
 
-function power() {
-	
+function power(a, b) {
+	return (Math.pow(a,b));	
 }
 
-function factorial() {
-	
+function factorial(someNumber) {
+	if (someNumber == 0 || someNumber == 1){
+		return 1;
+	}
+	else {
+		let total = 1;
+		while(someNumber > 1){
+			total *= someNumber;
+			someNumber--;
+		}
+		return total;
+	}	
 }
 
 module.exports = {
