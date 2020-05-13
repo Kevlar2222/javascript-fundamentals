f2580dc170a0e2f366e710e6f92df29aeab5f6b8
Kevlar2222 <kevin.satti@hotmail.co.uk>
2020-05-13 18:34:49 +0800

    fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = Number(index);
+    if(index < 0){
+        return("OOPS");
+    }
+    if(index == 1 || index == 2){
+        return 1;
+    }
+    let a = 1;
+    let b = 1;
+    let c;
+    for(let i = 0; i < index - 2; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return (c);
 }
 
 module.exports = fibonacci
