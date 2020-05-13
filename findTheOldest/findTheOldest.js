53da71ee0fafd50f50e472220551b218c0a1af77
Kevlar2222 <kevin.satti@hotmail.co.uk>
2020-05-13 19:03:20 +0800

    oldest people

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(array) {
+    const ordered = array.sort(function(a, b){
+        if(a.yearOfDeath === undefined){
+            let date = new Date();
+            a.yearOfDeath = date.getFullYear();
+        }
+        if(b.yearOfDeath === undefined){
+            let date = new Date();
+            b.yearOfDeath = date.getFullYear();
+        }
+        if(a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+    });
+    return ordered[0];
 }
 
 module.exports = findTheOldest
